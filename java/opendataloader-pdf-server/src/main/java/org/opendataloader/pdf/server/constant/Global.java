05910a72629b1142dcdabb65d8096c118edcba2b
UnicornLee <1571418313@qq.com>
2026-08-19 16:10:07 +0800

    fix: update annual report type code in Global class

--- java/opendataloader-pdf-server/src/main/java/org/opendataloader/pdf/server/constant/Global.java
+++ java/opendataloader-pdf-server/src/main/java/org/opendataloader/pdf/server/constant/Global.java
@@ -3,5 +3,5 @@ package org.opendataloader.pdf.server.constant;
 public class Global {
 
     // 年报类型
-    public static final String ANNUAL_REPORT_TYPE_CODE = "1785147629866030675";
+    public static final String ANNUAL_REPORT_TYPE_CODE = "1302577152400523280";
 }
